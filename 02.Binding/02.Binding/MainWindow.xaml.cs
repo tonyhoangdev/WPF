0d9976587d7e77ea7b831488ecd8c6d0e8c36c29
Tony Hoang <hoangtrongminh304@gmail.com>
2018-06-24 16:51:16 +0700

    add Binging

---
+++ 02.Binding/02.Binding/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02.Binding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+
+    {
+        private string myString;
+        public string MyString { get => myString; set { myString = value; OnPropertyChanged("MyString"); } }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyString = "My String";
+            this.DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string text)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(text));
+            }
+        }
+    }
+}
