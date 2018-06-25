fbc4f94f4c7f6014b3c90f995351431e17ddb6b4
Tony Hoang <hoangtrongminh304@gmail.com>
2018-06-25 11:30:37 +0700

    add Proxy, webrtc

--- Selenium/Selenium/MainWindow.xaml.cs
+++ Selenium/Selenium/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -31,16 +32,67 @@ namespace Selenium
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //FirefoxDriver fd = new FirefoxDriver();
+            //// ChromeDriver cd = new ChromeDriver();
 
-            ChromeDriver cd = new ChromeDriver();
-            cd.Url = "https://www.google.com";
-            cd.Navigate();
+            // profile
+            // turn off webRTC
+            FirefoxProfile firefoxProfile = new FirefoxProfile();
 
-            var search = cd.FindElementById("lst-ib");
-            search.SendKeys("c# WPF");
+            if ((bool)cbProxyWebRTC.IsChecked)
+            {
+                firefoxProfile.SetPreference("media.peerconnection.enabled", false);
+            }
 
-            search.SendKeys(Keys.Return);
+            if ((bool)cbProxyEnabled.IsChecked)
+            {
+                // changes proxy
+                string proxy_ip = txtProxyIp.Text;
+                int proxy_port = int.Parse(txtProxyPort.Text);
+
+                firefoxProfile.SetPreference("network.proxy.type", 1);
+                firefoxProfile.SetPreference("network.proxy.http", proxy_ip);
+                firefoxProfile.SetPreference("network.proxy.http_port", proxy_port);
+                firefoxProfile.SetPreference("network.proxy.ssl", proxy_ip);
+                firefoxProfile.SetPreference("network.proxy.ssl_port", proxy_port);
+            }
+
+            // init
+            FirefoxDriver cd = new FirefoxDriver(firefoxProfile);
+
+            if (!(bool)cbIsachInfo.IsChecked)
+            {
+                // go to link
+                if (!String.IsNullOrEmpty(txtLink.Text))
+                {
+                    cd.Url = txtLink.Text;
+                }
+                else
+                {
+                    cd.Url = "https://whoer.net/";
+                }
+
+                cd.Navigate();
+            }
+            else
+            {
+                cd.Url = "https://isach.info/login.php";
+                cd.Navigate();
+
+                var search = cd.FindElementById("user_name");
+                search.SendKeys(txtUser.Text);
+
+                search = cd.FindElementById("user_password");
+                search.SendKeys(passPass.Password);
+
+                search.SendKeys(Keys.Return);
+
+                // sleep
+                Thread.Sleep(4000);
+
+                // go home
+                cd.Url = "https://isach.info";
+                cd.Navigate();
+            }
 
         }
     }
