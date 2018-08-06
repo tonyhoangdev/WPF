b90d5f3454287933f8ab1453585e36594785662e
Tony Hoang <hoangtrongminh304@gmail.com>
2018-08-06 17:20:08 +0700

    add 10.Image

---
+++ 10.Image/10.Image/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _10.Image
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            img1.Width = 400;
+            img1.Source = new BitmapImage(new Uri("C:/Users/Tony/Documents/programming/WPF/10.Image/10.Image/image1.jpg"));
+        }
+    }
+}
