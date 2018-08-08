84435a46170ee2422956f31798f4137d2d4f3d30
Tony Hoang <hoangtrongminh304@gmail.com>
2018-08-09 00:40:46 +0700

    add 19.ValueConverter

---
+++ 19.ValueConverter/19.ValueConverter/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _19.ValueConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class AgeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime? selectedDate = value as DateTime?;
+
+            if (selectedDate == null)
+            {
+                return null;
+            }
+
+            return selectedDate.Value.Year;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int year = (int)value;
+            return new DateTime(year, 1, 1);
+        }
+    }
+}
